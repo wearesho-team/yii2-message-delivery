b3acecb0776d3fb8df0706fbf61fc4a647ac50e5
Alexander Letnikow <reclamme@gmail.com>
2018-05-15 10:53:18 +0300

    Test Repository

---
+++ tests/RepositoryTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Wearesho\Delivery\Yii2\Tests\Unit;
+
+use Wearesho\Delivery;
+
+/**
+ * Class RepositoryTest
+ * @package Wearesho\Delivery\Yii2\Tests\Unit
+ * @internal
+ */
+class RepositoryTest extends Delivery\Yii2\Tests\AbstractTestCase
+{
+    use Delivery\Test\RepositoryTest;
+
+    protected function getRepository(): Delivery\RepositoryInterface
+    {
+        return new Delivery\Yii2\Repository();
+    }
+}
