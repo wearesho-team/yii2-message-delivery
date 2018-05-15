593f4fecfad552f8b8fe56efe004e2f65a16a4d7
Alexander Letnikow <reclamme@gmail.com>
2018-05-15 10:53:04 +0300

    Add AbstractTestCase

---
+++ tests/AbstractTestCase.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Wearesho\Delivery\Yii2\Tests;
+
+use yii\phpunit\TestCase;
+
+/**
+ * Class AbstractTestCase
+ * @package Wearesho\Delivery\Yii2\Tests
+ * @internal
+ */
+class AbstractTestCase extends TestCase
+{
+
+}
