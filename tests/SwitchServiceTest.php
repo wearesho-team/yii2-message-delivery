19b32deaaedd761839302d2c8d6de3141edd5d75
Alexander Letnikow <reclamme@gmail.com>
2018-05-18 20:31:47 +0300

    Test SwitchService

---
+++ tests/SwitchServiceTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Wearesho\Delivery\Yii2\Tests;
+
+use yii\phpunit\TestCase;
+use yii\base;
+use Wearesho\Delivery;
+
+/**
+ * Class SwitchServiceTest
+ * @package Wearesho\Delivery\Yii2\Tests
+ */
+class SwitchServiceTest extends TestCase
+{
+    /** @var Delivery\ServiceMock[] */
+    protected $services;
+
+    /** @var Delivery\Yii2\SwitchService */
+    protected $switchService;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        for ($i = 0; $i < 2; $i++) {
+            $this->services[] = $service = new Delivery\ServiceMock;
+            $service->setRepository(new Delivery\MemoryRepository);
+        }
+
+        $this->switchService = new Delivery\Yii2\SwitchService([
+            'services' => $this->services,
+        ]);
+    }
+
+    public function testActiveService(): void
+    {
+        putenv('DELIVERY_SERVICE=0');
+        $this->assertEquals($this->services[0], $this->switchService->getActiveService());
+        putenv('DELIVERY_SERVICE=1');
+        $this->assertEquals($this->services[1], $this->switchService->getActiveService());
+        putenv('DELIVERY_SERVICE');
+
+        $this->expectException(base\InvalidConfigException::class);
+        $this->expectExceptionMessage('Service default does not configured.');
+
+        $this->switchService->getActiveService();
+    }
+
+    public function testSending(): void
+    {
+        putenv('DELIVERY_SERVICE=0');
+
+        $message = new Delivery\Message('text', 'recipient');
+        $this->switchService->send($message);
+
+        $this->assertTrue($this->services[0]->getRepository()->isSent($message));
+
+        putenv('DELIVERY_SERVICE');
+        $this->expectException(Delivery\Exception::class);
+        $this->expectExceptionMessage('Error while instantiating delivery service.');
+
+        $this->switchService->send($message);
+    }
+}
