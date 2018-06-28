559ba8064563cbe616f4cdaf0525f6ad5c7034ee
Alexander Letnikow <reclamme@gmail.com>
2018-06-28 13:56:05 +0300

    Implement console controller to send messages over CLI

---
+++ src/Console/Controller.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Wearesho\Delivery\Yii2\Console;
+
+use yii\console;
+use yii\di;
+use Wearesho\Delivery;
+
+/**
+ * Class Controller
+ * @package Wearesho\Delivery\Yii2\Console
+ */
+class Controller extends console\Controller
+{
+    /** @var string|array|Delivery\ServiceInterface definition */
+    public $delivery;
+
+    /**
+     * @param \yii\base\Action $action
+     * @return bool
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function beforeAction($action)
+    {
+        $this->delivery = di\Instance::ensure($this->delivery, Delivery\ServiceInterface::class);
+        return parent::beforeAction($action);
+    }
+
+    public function actionSend(string $message, string $text): void
+    {
+        /** @noinspection PhpUnhandledExceptionInspection */
+        $this->delivery->send(new Delivery\Message($text, $message));
+    }
+}
