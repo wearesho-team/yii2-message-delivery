20a87861bb6bdfa053dd43bc4584109ba528abc5
horat1us <reclamme+github@gmail.com>
2025-02-19 22:31:39 +0100

    Fix status column migration

--- src/Migrations/M250211000000AddStatusColumnToMessageDeliveryHistoryTable.php
+++ src/Migrations/M250211000000AddStatusColumnToMessageDeliveryHistoryTable.php
@@ -26,7 +26,7 @@ class M250211000000AddStatusColumnToMessageDeliveryHistoryTable extends Migratio
 
         $this->execute(<<<SQL
 UPDATE message_delivery_history
-SET status=(CASE WHEN is_sent IS TRUE THEN 'Sent' ELSE 'Failed' END)
+SET status=(CASE WHEN sent IS TRUE THEN 'Sent' ELSE 'Failed' END)
 SQL
         );
 
@@ -50,7 +50,7 @@ SQL
         );
         $this->execute(<<<SQL
 UPDATE message_delivery_history
-SET is_sent=(status='Delivered' OR status='Sent' OR status='Accepted' OR status='Read' OR status='Queued')
+SET sent=(status='Delivered' OR status='Sent' OR status='Accepted' OR status='Read' OR status='Queued')
 SQL
         );
         $this->alterColumn(
