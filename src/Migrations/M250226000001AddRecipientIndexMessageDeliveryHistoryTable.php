2b1c3a3478660bb6d1aeaf5d6120ec5fabe6bd0e
horat1us <reclamme+github@gmail.com>
2025-02-26 12:11:40 +0100

    Add index for message_delivery_history.recipient

--- src/Migrations/M250226000001AddRecipientIndexMessageDeliveryHistoryTable.php
+++ src/Migrations/M250226000001AddRecipientIndexMessageDeliveryHistoryTable.php
@@ -6,23 +6,17 @@ namespace Wearesho\Delivery\Yii2\Migrations;
 
 use yii\db\Migration;
 
-class M250211000003AddExternalIdColumnToMessageDeliveryHistoryTable extends Migration
+class M250226000001AddRecipientIndexMessageDeliveryHistoryTable extends Migration
 {
     private const TABLE_NAME = 'message_delivery_history';
-    private const COLUMN_NAME = 'external_id';
+    private const COLUMN_NAME = 'recipient';
 
     public function safeUp(): void
     {
-        $this->addColumn(
-            static::TABLE_NAME,
-            static::COLUMN_NAME,
-            $this->text()->null()
-        );
         $this->createIndex(
             $this->getIndexName(),
             static::TABLE_NAME,
-            ['sender', 'external_id'],
-            true
+            ['recipient'],
         );
     }
 
@@ -31,10 +25,6 @@ class M250211000003AddExternalIdColumnToMessageDeliveryHistoryTable extends Migr
         $this->dropIndex(
             $this->getIndexName(),
             static::TABLE_NAME,
-        );
-        $this->dropColumn(
-            static::TABLE_NAME,
-            static::COLUMN_NAME,
         );
     }
 
